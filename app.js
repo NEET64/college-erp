b872f27ea25aa12f84f6afa8dc1b188279718752
Neet <neetdhameliya321@gmail.com>
2024-01-13 20:20:09 +0530

    adding ui

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const port = 8000;
 const Course = require("./models/course");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://localhost:27017/erp";
 main()
@@ -19,6 +20,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/course", async (req, res) => {
   let courses = await Course.find({});
